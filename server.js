884adee18aca5dbb28db110aa7a9b12801512a48
Luís Henrique <luishenriquetp@gmail.com>
2023-10-22 13:05:57 -0300

    Criação CRUD com File System
    Criação CRUD com File System

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+http
+	.createServer((request, response) => {
+		response.writeHead(200, { 'Content-Type': 'application/json' });
+
+		if (request.url === '/produto') {
+			response.end(
+				JSON.stringify({
+					message: 'Rota de produto',
+				})
+			);
+		}
+
+		if (request.url === '/usuario') {
+			response.end(
+				JSON.stringify({
+					message: 'Rota de usuário',
+				})
+			);
+		}
+	})
+	.listen(4001, () => console.log('Servidor está rodando na porta 4001'));
